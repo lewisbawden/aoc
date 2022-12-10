c567d78b34b6bfb01e215b7061f579f63fc0a409
Lewis Bawden <lba@trackman.com>
2022-12-10 13:33:39 +0100

    updates

--- y2022/day_7_commented.py
+++ y2022/day_7_commented.py
@@ -4,15 +4,17 @@ import time
 
 class Dir:
     def __init__(self, name, parent):
-        self.name = name
-        self.parent = parent
-        self.dirs = {}
-        self.files = {}
-        self.size = 0
+        self.name = name  # string with the name of this Directory object
+        self.parent = parent  # Dir referring to the parent, for going up a level
+        self.dirs = {}  # dictionary with keys: values s.t. {'dirname': reference to another Dir object, ...}
+        self.files = {}  # dictionary s.t. {'filename': size as a float, ...}
+        self.size = 0  # size is the full directory size including all contents in children Dir objects
 
 
 class Browser:
     def __init__(self, limit):
+        # Initialise parameters, and initialise a root directory which has no parent
+        # self.current refers to the node currently being visited (the current working directory)
         self.current = Dir('/', None)
         self.limit = limit
         self.above_limit = []
@@ -22,49 +24,67 @@ class Browser:
         self.min_req_size = 0
 
     def readall(self, lines):
+        # Parse the lines one by one performing a relevant command
         for line in lines:
             self.read(line)
 
     def cd_root(self):
+        # The root directory is the only one with no parent - so keep going up until the current node has no parent
         while self.current.parent is not None:
             self.current = self.current.parent
 
     def getsizes(self):
+        # starting from the root node, get the size of each directory
+        # Getting a directories size requires knowing the size of any directories inside - so do this recursively
         self.cd_root()
         self.getsize(self.current)
         return sum(d.size for d in self.above_limit)
 
     def getsize(self, d):
+        # directory size is the sum of all files, and the sum of all contents in all child folders
         d.size = sum(d.files.values())
         for child in d.dirs.values():
+            # use the .getsize function to get the size of each child folder
             self.getsize(child)
+            # add the size of the child to the directory being evaluated
             d.size += child.size
         if d.size < self.limit:
             self.above_limit.append(d)
 
     def read(self, line):
         if line == '$ cd ..':
+            # set the current directory to its parent (go up one level)
             self.current = self.current.parent
         elif line == '$ cd /':
+            # go up a level until root is reached
             self.cd_root()
         elif line.startswith('$ cd'):
+            # change to a directory with this name
             dirname = line.split()[-1]
+            # add it as a new directory with self.current as the parent if it doesn't exist
             if dirname not in self.current.dirs.keys():
                 self.current.dirs[dirname] = Dir(dirname, self.current)
+            # change to it by setting the current directory to the Dir object with that name
             self.current = self.current.dirs[dirname]
         elif line.startswith('dir'):
+            # add the directory to the list of directories if it doesn't exist yet
             dirname = line.split()[-1]
             if dirname not in self.current.dirs.keys():
                 self.current.dirs[dirname] = Dir(dirname, self.current)
-        elif line != '$ ls':  # file
+        elif line != '$ ls':
+            # save file to the dictionary of files
             size, name = line.split()
             self.current.files[name] = float(size)
 
     def find_space(self, d):
+        # Check the space gained from deleting this - need to know the space gained from each child
         for child in d.dirs.values():
+            # check the space gained from each child using .find_space recursively
             self.find_space(child)
+        # check if the space gained is enough
         newsize = self.unused + d.size
         if newsize > self.need and d.size < self.min_req_size:
+            # if its enough, and its smaller than the current minimum, update the best answer so far
             self.min_req_size = d.size
 
 
